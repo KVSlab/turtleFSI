13f19bdc68a1dfff9cfa54832ef6d4e5ba7b03fb
Aslak Bergersen <aslak.bergersen@gmail.com>
2019-04-16 21:06:28 +0200

    Remove redundant functions in utils, Newtonsolver, and fluidvariation

--- modules/Newtonsolver/newtonsolver.py
+++ modules/Newtonsolver/newtonsolver.py
@@ -1,54 +1,63 @@
 from dolfin import *
-from numpy import isnan
-#import numpy as np
 
 
 def solver_setup(F_fluid_linear, F_fluid_nonlinear,
-                 F_solid_linear, F_solid_nonlinear, DVP, dvp_, **monolithic):
-
-    print("solver_setup")
+                 F_solid_linear, F_solid_nonlinear, DVP, dvp_, up_sol, **monolithic):
     F_lin = F_fluid_linear + F_solid_linear
-    F_nonlin = F_fluid_nonlinear + F_solid_nonlinear
+    F_nonlin = F_solid_nonlinear + F_fluid_nonlinear
     F = F_lin + F_nonlin
 
     chi = TrialFunction(DVP)
     J_linear = derivative(F_lin, dvp_["n"], chi)
     J_nonlinear = derivative(F_nonlin, dvp_["n"], chi)
 
-    #A_pre = assemble(J_linear)
-    A_pre = assemble(J_linear, form_compiler_parameters={"optimize": True, "quadrature_degree": 4})
+    A_pre = assemble(J_linear)  # , form_compiler_parameters = {"quadrature_degree": 4})
     A = Matrix(A_pre)
     b = None
 
-    return dict(F=F, J_nonlinear=J_nonlinear, A_pre=A_pre, A=A, b=b)
+    up_sol.parameters['reuse_factorization'] = True
+
+    return dict(F=F, J_nonlinear=J_nonlinear, A_pre=A_pre, A=A, b=b, up_sol=up_sol)
 
 
-def newtonsolver(F, J_nonlinear, A_pre, A, b, bcs, DVP,
+def newtonsolver(F, J_nonlinear, A_pre, A, b, bcs,
                  dvp_, up_sol, dvp_res, rtol, atol, max_it, T, t, **monolithic):
     Iter = 0
-    residual = 1
-    rel_res = residual
+    residual = 10**8
+    rel_res = 10**8
     lmbda = 1
+    last_rel_res = residual  # Capture if residual increases from last iteration
+    last_residual = rel_res
 
     while rel_res > rtol and residual > atol and Iter < max_it:
 
-        A = assemble(J_nonlinear, tensor=A, keep_diagonal=True, form_compiler_parameters={"optimize": True, "quadrature_degree": 4})
-        A.axpy(1.0, A_pre, True)
+        if Iter % 5 == 0 or (last_rel_res < rel_res and last_residual < residual and last_residual < rel_res):
+            print("assembling new JAC")
+            A = assemble(J_nonlinear, tensor=A,
+                         form_compiler_parameters={"quadrature_degree": 4},
+                         keep_diagonal=True)
 
-        A.ident_zeros()
-        b = assemble(-F, tensor=b, form_compiler_parameters={"quadrature_degree": 4})
-        [bc.apply(A, b, dvp_["n"].vector()) for bc in bcs]
+            A.axpy(1.0, A_pre, True)
+            A.ident_zeros()
+            [bc.apply(A) for bc in bcs]
+            up_sol.set_operator(A)
 
-        up_sol.solve(A, dvp_res.vector(), b)
+        b = assemble(-F, tensor=b)
 
+        last_rel_res = rel_res  # Capture if residual increases from last iteration
+        last_residual = residual
+
+        [bc.apply(b, dvp_["n"].vector()) for bc in bcs]
+        #[bc.apply(A, b, dvp_["n"].vector()) for bc in bcs]
+        up_sol.solve(dvp_res.vector(), b)
         dvp_["n"].vector().axpy(lmbda, dvp_res.vector())
         [bc.apply(dvp_["n"].vector()) for bc in bcs]
-
         rel_res = norm(dvp_res, 'l2')
         residual = b.norm('l2')
-        if isnan(rel_res) or isnan(residual):
-            print("type rel_res: ", type(rel_res))
-            t = T*T
+        if rel_res > 1E20 or residual > 1E20:
+            print("IN IF TEST")
+            t = T + 1
+            break
 
         if MPI.rank(mpi_comm_world()) == 0:
             print("Newton iteration %d: r (atol) = %.3e (tol = %.3e), r (rel) = %.3e (tol = %.3e) "
