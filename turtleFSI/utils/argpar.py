4e38c4f478e7c26f3562b06b051f985967bd3769
Aslak Bergersen <aslak.bergersen@gmail.com>
2019-04-18 23:46:54 +0200

    Bug fixes for argparse

--- turtleFSI/utils/argpar.py
+++ turtleFSI/utils/argpar.py
@@ -19,35 +19,35 @@ import argparse
 import string
 
 class StoreDictKeyPair(argparse.Action):
-     def __init__(self, option_strings, dest, nargs=None, **kwargs):
-         self._nargs = nargs
-         super(StoreDictKeyPair, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
+    def __init__(self, option_strings, dest, nargs=None, **kwargs):
+        self._nargs = nargs
+        super(StoreDictKeyPair, self).__init__(option_strings, dest, nargs=nargs, **kwargs)
 
-     def __call__(self, parser, namespace, values, option_string=None):
-         my_dict = {}
-         for kv in values:
-             k,v = kv.split("=")
-             try:
-                 # Int
-                 if set(v).issubset(set(string.digits)):
+    def __call__(self, parser, namespace, values, option_string=None):
+        my_dict = {}
+        for kv in values:
+            k,v = kv.split("=")
+            try:
+                # Int
+                if set(v).issubset(set(string.digits)):
                     my_dict[k] = int(v)
 
-                 # Float
-                 elif set(v).issubset(set(string.digis+".")):
-                     my_dict[k] = float(v)
+                # Float
+                elif set(v).issubset(set(string.digis+".")):
+                    my_dict[k] = float(v)
 
-                 # Boolean
-                 elif v.lower in ["true", "false"]:
-                     my_dict[k] = bool(v.capitalize())
+                # Boolean
+                elif v.lower in ["true", "false"]:
+                    my_dict[k] = bool(v.capitalize())
 
-                 # String
-                 else:
-                     my_dict[k] = v
+                # String
+                else:
+                    my_dict[k] = v
             except:
                 # String
                 my_dict[k] = v
 
-         setattr(namespace, self.dest, my_dict)
+        setattr(namespace, self.dest, my_dict)
 
 
 def str2bool(boolean):
@@ -83,12 +83,12 @@ def parse():
     parser = argparse.ArgumentParser(description="TODO: Full description of the entire turtleFSI")
 
     # Define solver, numerics, and problem file
-    parser.add_argument("-p", "--problem", type=str, default=None, meta_var="Problem",
+    parser.add_argument("-p", "--problem", type=str, default=None, metavar="Problem",
                         help="Name of problem file to solve. Could either be loced in the" +
                         " turtleFSI repository (TF_cfd, TF_csm, TF_fsi, turtle_demo) or it" +
                         " could be a problem file you have created locally.")
     parser.add_argument("-t", "--theta", type=restricted_float, default=None,
-                        meta_var="Theta",
+                        metavar="Theta",
                         help="Setting temporal integration. For theta = 0 the scheme is" +
                         " explicit, thetha = 1 the scheme is implicit, and for theta = 0.5" +
                         " it becommes a second-order Crank-Nicolson. Of note is that theta =" +
@@ -97,27 +97,27 @@ def parse():
 
     # Set fluid, solid, and extrapolation
     parser.add_argument("-f", "--fluid", type=str, default=None,
-                        choices=["fluid", "no-fluid"], meta_var="Fluid"
+                        choices=["fluid", "no-fluid"], metavar="Fluid",
                         help="Turn off fluid and only solve the structure equation")
-    parser.add_argument("-s", "--solid", type=str, default=None, meta_var="Solid",
+    parser.add_argument("-s", "--solid", type=str, default=None, metavar="Solid",
                         choices=["solid", "no-solid"],
                         help="Turn off solid and only solve the fluid equation")
     parser.add_argument("-e", "--extrapolation", type=str, default=None,
-                        meta_var="Extrapolation method",
+                        metavar="Extrapolation method",
                         choices=["laplace", "linear", "biharmonic", "no-extrapolation"],
                         help="Set approach for extrapolating the deformation into the fluid" +
                             "domain")
     parser.add_argument("-et", "--extrapolation-sub-type", type=str,
-                        meta_var="Extrapolation sub type", default=None,
+                        metavar="Extrapolation sub type", default=None,
                         choices=["constant", "small_constant", "volume", "volume_change",
                                  "bc1", "bc2"],
                         help="Set the sub type of the extrapolation method. TODO")
-    parser.add_argument("--bc_ids", nargs="+", type=int, default=None, meta_var="ID list",
+    parser.add_argument("--bc_ids", nargs="+", type=int, default=None, metavar="ID list",
                         help="List of boundary ids for the weak formulation of the" +
                         " biharmonic mesh lifting operator with 'bc2'")
 
     # Meterial settings / physical constants
-    parser.add_argument("--Um", type=float, default=None;
+    parser.add_argument("--Um", type=float, default=None,
                         help="Maximum velocity at inlet")
     parser.add_argument("--rho_f", type=float, default=None,
                         help="Density of the fluid")
@@ -149,19 +149,19 @@ def parse():
                         help="Choose the linear solver for each Newton iteration," +
                         " to see a complete list run list_linear_solvers()")
     parser.add_argument("--atol", type=float, default=None,
-                        meta_var="Absolute tolerance",
+                        metavar="Absolute tolerance",
                         help="The absolute tolerance for the Newton iterations")
     parser.add_argument("--rtol", type=float, default=None,
-                        meta_var="Relative tolerance",
+                        metavar="Relative tolerance",
                         help="The relative tolerance for the Newton iterations")
     parser.add_argument("--max_it", type=int, default=None,
-                        meta_var="Maximum iterations",
+                        metavar="Maximum iterations",
                         help="Maximum number of iterations in the Newton solver")
     parser.add_argument("--lmbda", type=restricted_float, default=None,
-                        meta_var="Relaxation factor",
+                        metavar="Relaxation factor",
                         help="Relaxation factor in the Netwon solver")
     parser.add_argument("--recompute", type=int, default=None,
-                        meta_var="Recompute Jacobian",
+                        metavar="Recompute Jacobian",
                         help="How often to recompute the Jacobian.")
     parser.add_argument("--compiler-parameters", dest="compiler_parameters",
                         action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL",
@@ -180,35 +180,36 @@ def parse():
                         " defined in the problem file")
 
     # Set spatial and temporal resolution
-    parser.add_argument("-dt", meta_var="Time step", type=float,
+    parser.add_argument("-dt", metavar="Time step", type=float,
                         help="Set timestep, dt", default=None)
-    parser.add_argument("-T", type=float, meta_var="End time",
+    parser.add_argument("-T", type=float, metavar="End time",
                         help="Set end time", default=None)
-    parser.add_argument("--p_deg", meta_var="Pressure degree", type=int,
+    parser.add_argument("--p_deg", metavar="Pressure degree", type=int,
                         help="Set degree of pressure", default=None)
-    parser.add_argument("--v_deg", meta_var="Velocity degree", type=int,
+    parser.add_argument("--v_deg", metavar="Velocity degree", type=int,
                         help="Set degree of velocity", default=None)
-    parser.add_argument("--d_deg", meta_var="Deformation degree", type=int,
+    parser.add_argument("--d_deg", metavar="Deformation degree", type=int,
                         help="Set degree of deformation", default=None)
 
     # Add the posibility pass unspecificed arguments
-    parser.add_argument("--new-arguments", dest="rest_str", type=int,
-                        action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL",
+    parser.add_argument("--new-arguments", action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL",
                         help="Add any non-defined argument where the value is a string," +
                         " by providing a key=value, e.g. folder=TF_fsi_results. You can provide" +
                         " multiple key=value pairs seperated by a whitespace",
                         default=None)
 
+    # Parse arguments
     args = parser.parse_args()
 
     # Add unspecificed arguments
-    args.__dict__.update(args.new_arguments)
-    args.__dict__.pop("new_arguments")
+    if args.new_arguments is not None:
+        args.__dict__.update(args.new_arguments)
+        args.__dict__.pop("new_arguments")
 
     # Update the default values and then set the entire dictionary to be the inpute from
     # arparse
     if args.compiler_parameters is not None:
-        from turtleFSI.problems import *
+        from turtleFSI.problems import default_variables
         default_variables["compiler_parameters"].update(args.compiler_parameters)
         args.compiler_parameters = default_variables["compiler_parameters"]
 
