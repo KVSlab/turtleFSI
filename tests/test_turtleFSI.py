c501f054a094de3225be6b311c5d0db0b2b32d09
Aslak Bergersen <aslak.bergersen@gmail.com>
2019-04-24 22:58:20 +0200

    Working tests

---
+++ tests/test_turtleFSI.py
@@ -0,0 +1,122 @@
+# Co (c) XXXX, XXXX.
+# See LICENSE file for details.
+
+# This software is distributed WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.
+
+import pytest
+import numpy as np
+from os import system
+
+
+def test_cfd():
+    cmd = ("turtleFSI --problem TF_cfd -dt 0.01 -T 0.05 --verbose True" +
+           " --new-arguments folder=tmp")
+    d = system(cmd)
+
+    drag = np.loadtxt("tmp/Drag.txt")[-1]
+    lift = np.loadtxt("tmp/Lift.txt")[-1]
+    drag_reference = 2.5637554331614054
+    lift_reference = -0.02078995609237899
+
+    assert drag == drag_reference
+    assert lift == lift_reference
+
+
+def test_csm():
+    cmd = ("turtleFSI --problem TF_csm -dt 0.01 -T 0.05 --verbose True" +
+           " --new-arguments folder=tmp")
+    d = system(cmd)
+
+    distance_x = np.loadtxt("tmp/dis_x.txt")[-1]
+    distance_y = np.loadtxt("tmp/dis_y.txt")[-1]
+    distance_x_reference = -6.13487990897633e-06
+    distance_y_reference = -3.9398599897576816e-05
+
+    assert distance_x == distance_x_reference
+    assert distance_y == distance_y_reference
+
+
+def test_fsi():
+    cmd = ("turtleFSI --problem TF_fsi -dt 0.01 -T 0.05 --verbose True --theta 0.51" +
+           " --new-arguments folder=tmp")
+    d = system(cmd)
+
+    drag = np.loadtxt("tmp/Drag.txt")[-1]
+    lift = np.loadtxt("tmp/Lift.txt")[-1]
+    distance_x = np.loadtxt("tmp/dis_x.txt")[-1]
+    distance_y = np.loadtxt("tmp/dis_y.txt")[-1]
+    distance_x_reference = -3.0193475393178104e-06
+    distance_y_reference = -2.6614039656203487e-08
+    drag_reference = 2.4729291261050355
+    lift_reference = -0.003952155852968209
+
+    assert distance_x == distance_x_reference
+    assert distance_y == distance_y_reference
+    assert drag == drag_reference
+    assert lift == lift_reference
+
+
+@pytest.mark.parametrize("extrapolation_sub_type", ["volume", "volume_change",
+                                                    "constant", "small_constant"])
+def test_laplace(extrapolation_sub_type):
+    cmd = ("turtleFSI --problem TF_fsi -dt 0.01 -T 0.05 --verbose True --theta 0.51"
+           " --new-arguments folder=tmp")
+    d = system(cmd)
+
+    drag = np.loadtxt("tmp/Drag.txt")[-1]
+    lift = np.loadtxt("tmp/Lift.txt")[-1]
+    distance_x = np.loadtxt("tmp/dis_x.txt")[-1]
+    distance_y = np.loadtxt("tmp/dis_y.txt")[-1]
+    distance_x_reference = -3.0193475393178104e-06
+    distance_y_reference = -2.6614039656203487e-08
+    drag_reference = 2.4729291261050355
+    lift_reference = -0.003952155852968209
+
+    assert distance_x == distance_x_reference
+    assert distance_y == distance_y_reference
+    assert drag == drag_reference
+    assert lift == lift_reference
+
+
+@pytest.mark.parametrize("extrapolation_sub_type", ["bc1", "bc2"])
+def test_biharmonic(extrapolation_sub_type):
+    cmd = ("turtleFSI --problem TF_fsi -dt 0.01 -T 0.05 --verbose True --theta 0.51" +
+           "--extrapolation biharmonic --extrapolation_sub_type" +
+           " --new-arguments folder=tmp {}".format(extrapolation_sub_type))
+    d = system(cmd)
+
+    drag = np.loadtxt("tmp/Drag.txt")[-1]
+    lift = np.loadtxt("tmp/Lift.txt")[-1]
+    distance_x = np.loadtxt("tmp/dis_x.txt")[-1]
+    distance_y = np.loadtxt("tmp/dis_y.txt")[-1]
+    distance_x_reference = -3.0193475393178104e-06
+    distance_y_reference = -2.6614039656203487e-08
+    drag_reference = 2.4729291261050355
+    lift_reference = -0.003952155852968209
+
+    assert distance_x == distance_x_reference
+    assert distance_y == distance_y_reference
+    assert drag == drag_reference
+    assert lift == lift_reference
+
+
+def test_elastic():
+    cmd = ("turtleFSI --problem TF_fsi -dt 0.01 -T 0.05 --verbose True --theta 0.51" +
+           " --extrapolation linear --new-arguments folder=tmp")
+    d = system(cmd)
+
+    drag = np.loadtxt("tmp/Drag.txt")[-1]
+    lift = np.loadtxt("tmp/Lift.txt")[-1]
+    distance_x = np.loadtxt("tmp/dis_x.txt")[-1]
+    distance_y = np.loadtxt("tmp/dis_y.txt")[-1]
+    distance_x_reference = -3.0193475393178104e-06
+    distance_y_reference = -2.6614039656203487e-08
+    drag_reference = 2.4729291261050355
+    lift_reference = -0.003952155852968209
+
+    assert distance_x == distance_x_reference
+    assert distance_y == distance_y_reference
+    assert drag == drag_reference
+    assert lift == lift_reference
