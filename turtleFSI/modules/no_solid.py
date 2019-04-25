d2e6497374848571281a58ff6300f45d496108ef
Aslak Bergersen <aslak.bergersen@gmail.com>
2019-04-25 12:01:07 +0200

    Fix indentation error

--- turtleFSI/modules/no_solid.py
+++ turtleFSI/modules/no_solid.py
@@ -9,11 +9,8 @@ from dolfin import Constant, inner
 
 
 def solid_setup(psi, phi, dx_s, mesh, **namespace):
-    """
-    No contribution from the structure, for when solving only the fluid equation.
-    """
+    """No contribution from the structure, for when solving only the fluid equation."""
+    F_solid_linear = inner(Constant(tuple([0]*mesh.geometry().dim())), psi)*dx_s
+    F_solid_nonlinear = inner(Constant(tuple([0]*mesh.geometry().dim())), phi)*dx_s
 
-	F_solid_linear = inner(Constant(tuple([0]*mesh.geometry().dim())), psi)*dx_s
-	F_solid_nonlinear = inner(Constant(tuple([0]*mesh.geometry().dim())), phi)*dx_s
-
-	return dict(F_solid_linear=F_solid_linear, F_solid_nonlinear=F_solid_nonlinear)
+    return dict(F_solid_linear=F_solid_linear, F_solid_nonlinear=F_solid_nonlinear)
