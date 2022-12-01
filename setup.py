4622de6820136f63e272f11f1c7835ce4e4dda9d
Jørgen S. Dokken <dokken@simula.no>
2022-12-01 12:57:56 +0100

    Try less deps

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-DEPENDENCIES = ['configargparse', "fenics-dolfin", "numpy",
+DEPENDENCIES = ['configargparse', "fenics-dolfin",
                 "scipy", "cppimport"]
 TEST_DEPENDENCIES = ['pytest']
 
