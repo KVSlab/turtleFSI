f1b1f444c9e45f3d6db24717c58da6cf9c7b8e0f
Jørgen S. Dokken <dokken@simula.no>
2022-12-09 10:18:07 +0100

    Use scientificcomputing image with python 3.10 for testing

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 DEPENDENCIES = ['configargparse', "fenics-dolfin",
-                "scipy", "cppimport"]
+                "scipy", "cppimport", "numpy"]
 TEST_DEPENDENCIES = ['pytest']
 
 VERSION = "1.5"
