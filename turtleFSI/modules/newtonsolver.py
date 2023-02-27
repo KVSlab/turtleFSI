cc12c234ca1e7ddac4614a6ab4745bf2bc63f5ba
Kei <withwhom0215@gmail.com>
2023-02-27 09:14:29 +0100

    update newtonsolver and add documentation about the known issue

--- turtleFSI/modules/newtonsolver.py
+++ turtleFSI/modules/newtonsolver.py
@@ -3,8 +3,10 @@
 # the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 # PURPOSE.
 
-from dolfin import assemble, derivative, TrialFunction, Matrix, norm, MPI
+from dolfin import assemble, derivative, TrialFunction, Matrix, norm, MPI, PETScOptions
 
+PETScOptions.set("mat_mumps_icntl_4", 1) # If negatvie or zero, MUMPS will suppress diagnositc printining, statistics, and warning messages. 
+PETScOptions.set("mat_mumps_icntl_14", 400) # allocate more memory to mumps
 
 def solver_setup(F_fluid_linear, F_fluid_nonlinear, F_solid_linear, F_solid_nonlinear,
                  DVP, dvp_, up_sol, compiler_parameters, **namespace):
@@ -43,7 +45,6 @@ def newtonsolver(F, J_nonlinear, A_pre, A, b, bcs, lmbda, recompute, recompute_t
     iter = 0
     residual = 10**8
     rel_res = 10**8
-
     # Capture if residual increases from last iteration
     last_rel_res = residual
     last_residual = rel_res
